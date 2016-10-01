2c841bb5150b2811a10c0c3d0ca69e83aba0b362
tim70036 <tim70036@gmail.com>
2016-10-01 23:37:00 +0800

    Init

---
+++ code/userprog/ksyscall.h
@@ -0,0 +1,38 @@
+/**************************************************************
+ *
+ * userprog/ksyscall.h
+ *
+ * Kernel interface for systemcalls 
+ *
+ * by Marcus Voelp  (c) Universitaet Karlsruhe
+ *
+ **************************************************************/
+
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
+
+#include "kernel.h"
+
+#include "synchconsole.h"
+
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+int SysCreate(char *filename)
+{
+	// return value
+	// 1: success
+	// 0: failed
+	return kernel->interrupt->CreateFile(filename);
+}
+
+
+#endif /* ! __USERPROG_KSYSCALL_H__ */
