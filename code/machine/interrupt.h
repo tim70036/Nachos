923a3f77ff0919e0e81f8cbf2cd8e7307784856e
tim70036 <tim70036@gmail.com>
2016-10-02 23:13:43 +0800

    PrintInt fuckNachOS

--- code/machine/interrupt.h
+++ code/machine/interrupt.h
@@ -94,7 +94,6 @@ class Interrupt {
 
     void Halt(); 		// quit and print out stats
 
-    void PrintInt(int number);
 	int CreateFile(char *filename);
 
     void YieldOnReturn();	// cause a context switch on return
