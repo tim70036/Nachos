2efe146251d50daa4e7a042768a5c1d793a99a91
tim70036 <tim70036@gmail.com>
2017-01-15 23:05:17 +0800

    MP4 Test case fix

--- code/test/FS_2.sh
+++ code/test/FS_2.sh
@@ -1,4 +1,4 @@
 ../build.linux/nachos -f
-../build.linux/nachos -cp emptyFile.test /BigEmptyFile
+../build.linux/nachos -cp FS_empty.test /BigFile
 ../build.linux/nachos -lr /
 echo "========================================="
